f2884e72c4c733d80ea1a6f14cedd64d538b8200
Paptoos <72940447+Paptoos@users.noreply.github.com>
2020-10-31 16:30:17 +0330

    Terminal , Just Open FD files and // coments
    3 options added 
    1.If the File not pass to programm , user go to Terminal to Programming live , 
    2. Just Open Files with .fd format
    3. // coments to use

--- interpreter.py
+++ interpreter.py
@@ -33,6 +33,10 @@ class PPLLexer(Lexer):
 	@_(r'#.*')
 	def COMMENT(self, t):
 		pass
+	
+	@_(r'//.*')
+	def COMMENT(self, t):
+		pass
 
 	@_(r'\n+')
 	def newline(self, t):
@@ -219,13 +223,20 @@ class PPLExecute(object):
 			return (self.walk_tree(node[1]), self.walk_tree(node[2]))
 
 if __name__ == '__main__':
-	lexer = PPLLexer()
-	parser = PPLParser()
-	env = {}
-	if len(argv) < 2:
-		print('باید آدرس فایل برنامه را به‌عنوان آرگومنت به برنامه بدهید.')
-	with open(argv[1], encoding="utf-8") as f:
-		for line in f.read().splitlines():
-			tokens = lexer.tokenize(line)
-			tree = parser.parse(tokens)
-			PPLExecute(tree, env)
+    lexer = PPLLexer()
+    parser = PPLParser()
+    env = {}
+    if len(argv) < 2:
+        while True:
+            test = input('Ferdosi >>> ')
+            tokens = lexer.tokenize(test)
+            tree = parser.parse(tokens)
+            PPLExecute(tree, env)
+    elif argv[1][-1] == 'd' and argv[1][-2] == 'f' and argv[1][-3] == '.':
+        with open(argv[1], encoding="utf-8") as f:
+            for line in f.read().splitlines():
+                tokens = lexer.tokenize(line)
+                tree = parser.parse(tokens)
+                PPLExecute(tree, env)
+    else:
+        print('باید فایل دارای پسوند fd باشد')
